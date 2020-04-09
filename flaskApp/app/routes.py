321e862bc0696c7a3da7f84a5739d38304008414
emconneilly <emconneilly@wpi.edu>
2020-04-09 09:06:05 -0400

    Added basic prediction to the webpage

--- flaskApp/app/routes.py
+++ flaskApp/app/routes.py
@@ -23,36 +23,44 @@ def createModel():
     config = api.createConfig(kernel, gamma, nu)
     # Train Model
     model = api.train(train, config)
-    # Perform Tests Requested
-    if 'self' in request.form:
-        summary = api.testSelfWeb(model, train, numSequences)
-    if 'consensus' in request.form:
-        consensus, numSequences = api.prepFile('app/static/consensusTest.csv')
-        summary = api.testData(model, consensus, 'app/static/consensusTestLabels.csv')
-    if 'mixNES' in request.form:
-        mixNES, numSequences = api.prepFile('app/static/mixNESTest.csv')
-        summary = api.testData(model, mixNES, 'app/static/mixNESTestLabels.csv')
-    if 'mixWithRandom' in request.form:
-        mixWithRandom, numSequences = api.prepFile('app/static/mixWithRandomTest.csv')
-        summary = api.testData(model, mixWithRandom, 'app/static/mixWithRandomTestLabels.csv')
-    if 'mixAll' in request.form:
-        mixAll, numSequences = api.prepFile('app/static/mixAllTest.csv')
-        summary = api.testData(model, mixAll, 'app/static/mixAllTestLabels.csv')
-    if 'custom' in request.form:
-        testFile = request.files['testFile']
-        testLabels = request.files['testLabels']
-        # Save custom input to uploads
-        testFile_name = testFile.filename
-        testFile_path = os.path.join('uploads/', testFile_name)
-        testFile.save(testFile_path)
-        testLabels_name = testLabels.filename
-        testLabels_path = os.path.join('uploads/', testLabels_name)
-        testLabels.save(testLabels_path)
-        # Run test
-        custom, numSequences = api.prepFile(testFile_path)
-        summary = api.testData(model, custom, testLabels_path)
-    
-    return render_template('createModel.html', summary=summary)
+    # Check if testing or predicting
+    if 'self' in request.form or 'consensus' in request.form or 'mixNES' in request.form or 'mixWithRandom' in request.form or 'mixAll' in request.form or 'custom' in request.form:
+        # Perform Tests Requested
+        if 'self' in request.form:
+            summary = api.testSelfWeb(model, train, numSequences)
+        if 'consensus' in request.form:
+            consensus, numSequences = api.prepFile('app/static/consensusTest.csv')
+            summary = api.testData(model, consensus, 'app/static/consensusTestLabels.csv')
+        if 'mixNES' in request.form:
+            mixNES, numSequences = api.prepFile('app/static/mixNESTest.csv')
+            summary = api.testData(model, mixNES, 'app/static/mixNESTestLabels.csv')
+        if 'mixWithRandom' in request.form:
+            mixWithRandom, numSequences = api.prepFile('app/static/mixWithRandomTest.csv')
+            summary = api.testData(model, mixWithRandom, 'app/static/mixWithRandomTestLabels.csv')
+        if 'mixAll' in request.form:
+            mixAll, numSequences = api.prepFile('app/static/mixAllTest.csv')
+            summary = api.testData(model, mixAll, 'app/static/mixAllTestLabels.csv')
+        if 'custom' in request.form:
+            testFile = request.files['testFile']
+            testLabels = request.files['testLabels']
+            # Save custom input to uploads
+            testFile_name = testFile.filename
+            testFile_path = os.path.join('uploads/', testFile_name)
+            testFile.save(testFile_path)
+            testLabels_name = testLabels.filename
+            testLabels_path = os.path.join('uploads/', testLabels_name)
+            testLabels.save(testLabels_path)
+            # Run test
+            custom, numSequences = api.prepFile(testFile_path)
+            summary = api.testData(model, custom, testLabels_path)
+            
+        return render_template('createModel.html', summary=summary)
+    # If predicting, read in sequence and predict sequences
+    miniSequences = api.getSequencesFromString(request.form['predictSequence'])
+    preppedPredict = api.prepText(miniSequences)
+    functional = api.predict(model, preppedPredict, miniSequences)
+
+    return render_template('prediction.html', functional=functional)
 
 @app.route('/static/<path:filename>')
 def get_file(filename):
